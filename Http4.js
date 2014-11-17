29235cbd258b82cf8b8af99151c4b6cd0c3e805d
victoryfree <victoryfree@hanmail.net>
2014-11-17 23:12:24 +0900

    Http4.js change code

--- Http4.js
+++ Http4.js
@@ -1,42 +1,5 @@
 var http = require('http');
-var fs = require('fs');
-
-// 숫자를 쓴다
-function writeNumbers(res) {
-
-	var counter = 0;
-
-	// 숫자를 증가해가면서 클라이언트에게 쓴다
-	for (var i =0; i<100; i++) {
-		counter++;
-		res.write(counter.toString() + '\n');
-	}
-}
-
-// 서버 생성
-http.createServer(function (req, res) {
-	var query = require('url').parse(req.url) query;
-	var qpp = require('querystring').parse(query).file + ".txt";
-
-
-	// 컨텐츠 헤더
-	res.writeHead(200, {'Context-Type': 'text/plain'});
+// 원하는 url과 필요한 경로 및 옵션
+var options = {
 	
-	// 숫자를 쓴다
-	writeNumbers(res);
-
-	// 파일을 열고 내용을 읽기 위한 타이머
-	setTimeout(function() {
-
-		console.log('opening ' + app);
-		// 파일 내용을 열고 읽음
-		fs.readFile(app, 'utf8', funcion(err, data) {
-			if (err)
-				res.write('Could not find or open file for reading\n');
-			else
-				res.write(data);
-			}
-			// 응답 완료
-		})
-	}
-})
+}
